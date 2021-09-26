94d6213d9721e359fe3ae3007195830958403cb1
Austinbreathesair <austinbreathesair@gmail.com>
2021-09-26 19:48:33 +0200

    initial commit: created homepage template.

---
+++ public/js/nav.js
@@ -0,0 +1,26 @@
+const createNav = () => {
+  let nav = document.querySelector('.navbar');
+
+  nav.innerHTML = `
+          <div class="nav">
+    <img src="img/dark-logo.png" class="brand-logo" alt="">
+    <div class="nav-items">
+      <div class="search">
+        <input type="text" class="search-box" placeholder="Search brand, product, etc.">
+        <button class="search-btn">Search</button>
+      </div>
+      <a href="#"><img src="img/user.png" alt=""></a>
+      <a href="#"><img src="img/cart.png" alt=""></a>
+    </div>
+  </div>
+  <ul class="links-container">
+    <li class="link-item"><a href="#" class="link">Home</a></li>
+    <li class="link-item"><a href="#" class="link">Ladies</a></li>
+    <li class="link-item"><a href="#" class="link">Men</a></li>
+    <li class="link-item"><a href="#" class="link">Kids</a></li>
+    <li class="link-item"><a href="#" class="link">Accessories</a></li>
+  </ul>
+  `;
+}
+
+createNav();
